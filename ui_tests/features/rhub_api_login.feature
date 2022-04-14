5da60e28850688fdbd40b69885fddb56d008bf32
Allan da Silva Barroso <abarroso@redhat.com>
2022-04-14 05:41:28 -0300

    Login rhub_api

--- ui_tests/features/rhub_api_login.feature
+++ ui_tests/features/rhub_api_login.feature
@@ -2,9 +2,9 @@
 Feature: Login execution for rhub api
     In order to access the system throught the api
     As a given of user I want to be able to acces the system by the api 
-    So that reason
+    So I can be connected with a valid token
 
 Scenario:
-     Given I connect to the endpoint
+     Given I request the token
      When  I execute the authentication
-     Then  I must be logged in the system
+     Then  I must have access in the system
--- ui_tests/features/steps/rhub_api_login.py
+++ ui_tests/features/steps/rhub_api_login.py
@@ -1,13 +1,27 @@
-@given(u'I connect to the endpoint')
+from lib2to3.pgen2 import token
+from helpers import rhub_api_connection
+
+auth = AUTH_USER = ('testuser1', 'testuser1')
+
+token_f = rhub_api_connection.request_token(auth)
+
+
+@given(u'I request the token')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Given I connect to the endpoint')
+    
+    assert(token_f != '')
 
 
+    
 @when(u'I execute the authentication')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I execute the authentication')
+    resp = rhub_api_connection.request_response(context, token_f)
+    assert(resp != '')
 
 
-@then(u'I must be logged in the system')
+@then(u'I must have access in the system')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I must be logged in the system')
+
+    recal = rhub_api_connection.request_response(context, token_f)
+    print(recal)
+    assert(recal == 200)
