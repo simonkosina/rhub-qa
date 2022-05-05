c02a1ec3a3ed3f4712b6187914e2b57f85e4aeed
skosina <skosina@redhat.com>
2022-05-05 10:03:23 +0200

    Creates page classes

---
+++ ui_tests/features/steps/helpers/login_page.py
@@ -0,0 +1,14 @@
+from helpers.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class LoginPage(BasePage):
+    """
+    Page class for the RHub login page.
+    """
+
+    locators = {
+        "login_btn": (By.XPATH, "/html/body/div/div/main/div/div[1]/div[2]/button"),
+        "sign_in_btn": (By.ID, "kc-login"),
+        "username": (By.ID, "username"),
+        "password": (By.ID, "password")
+    }
