4a106d12b7762b92b0523aec49adc03545b7939e
skosina <skosina@redhat.com>
2022-05-05 10:04:02 +0200

    Updates tests to use page classes

--- ui_tests/features/steps/rhub_login.py
+++ ui_tests/features/steps/rhub_login.py
@@ -2,6 +2,10 @@ from helpers.login_page import LoginPage
 from helpers.main_page import MainPage
 from behave import *
 
+from helpers.login_page import LoginPage
+from helpers.main_page import MainPage
+from behave import *
+
 @given(u'web address "{url}"')
 def step_impl(context, url):
     login_page = LoginPage(context)
