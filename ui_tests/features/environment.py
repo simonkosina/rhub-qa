17a148c6ecd27f11568580a3d9ab1750ac9082db
skosina <skosina@redhat.com>
2022-05-19 09:05:16 +0200

    Fixes indentation

--- ui_tests/features/environment.py
+++ ui_tests/features/environment.py
@@ -1,56 +1,53 @@
-from selenium import webdriver
 import time
 import os
 
- 
-t = time.localtime()     
+from selenium import webdriver
+
+t = time.localtime()
+
 
 def before_scenario(context, scenario):
-  
-  vname = context.scenario.name + str(time.strftime("%H:%M:%S", t))
 
-  if 'web' in context.tags:
-      os.system('./../tools/cm selenoid start')
-      os.system('./../tools/cm selenoid-ui start')  
-      time.sleep(5)
-      
-      
+    vname = context.scenario.name + str(time.strftime("%H:%M:%S", t))
 
-      capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "99.0",
-        "acceptInsecureCerts": True,
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True,
-            "videoName": vname + ".mp4", 
-            "startMaximized": True
-                        }
-                            }
+    if 'web' in context.tags:
+        os.system('./../tools/cm selenoid start')
+        os.system('./../tools/cm selenoid-ui start')
+        time.sleep(5)
 
-      context.browser = webdriver.Remote(
-      command_executor="http://localhost:4444/wd/hub",
-      desired_capabilities=capabilities)
+        capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "99.0",
+            "acceptInsecureCerts": True,
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True,
+                "videoName": vname + ".mp4",
+                "startMaximized": True
+            }
+        }
 
-      context.browser.implicitly_wait(10)
-  
-     
+        context.browser = webdriver.Remote(
+            command_executor="http://localhost:4444/wd/hub",
+            desired_capabilities=capabilities)
 
- 
+        context.browser.implicitly_wait(10)
+
+
 def after_scenario(context, scenario):
-  if 'web' in context.tags:
-    context.browser.quit()
-    
+    if 'web' in context.tags:
+        context.browser.quit()
+
 
 def after_all(context):
-      
-      time.sleep(5)
-      os.system('./../tools/cm selenoid stop')
-      os.system('./../tools/cm selenoid-ui stop')
-      os.system('cp ~/.aerokube/selenoid/video/*.mp4 ~/Vídeos/')
-      
-      print(" ")
-      print("--------------------------------------------")
-      print(" ")
-      print("Tests results: ")
-      print("    ")
+    
+    time.sleep(5)
+    os.system('./../tools/cm selenoid stop')
+    os.system('./../tools/cm selenoid-ui stop')
+    os.system('cp ~/.aerokube/selenoid/video/*.mp4 ~/Vídeos/')
+
+    print(" ")
+    print("--------------------------------------------")
+    print(" ")
+    print("Tests results: ")
+    print("    ")
