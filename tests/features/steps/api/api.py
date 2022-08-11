c752b3161325433ff7ebe2c8617b600adaf931c8
skosina <skosina@redhat.com>
2022-08-11 10:00:41 +0200

    Add infrastructure for /scheduler API endpoint

--- tests/features/steps/api/api.py
+++ tests/features/steps/api/api.py
@@ -9,6 +9,7 @@ from api.policies_endpoint import PoliciesEndpoint
 from api.ping_endpoint import PingEndpoint
 from api.cowsay_endpoint import CowsayEndpoint
 from api.openstack_endpoint import OpenstackEndpoint
+from api.scheduler_endpoint import SchedulerEndpoint
 
 
 class API(object):
@@ -31,6 +32,7 @@ class API(object):
         self.ping = PingEndpoint(self.session)
         self.cowsay = CowsayEndpoint(self.session)
         self.me = MeEndpoint(self.session)
+        self.scheduler = SchedulerEndpoint(self.session)
 
     def update_token(self, access_token: str):
         """
