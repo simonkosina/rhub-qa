fad0422abfa0716bc5a3e39e667d126d4501435c
Simon Košina <52234617+simonkosina@users.noreply.github.com>
2022-08-11 13:51:09 +0200

    Add infrastructure for /dns API endpoint (#16)

--- tests/features/steps/api/api.py
+++ tests/features/steps/api/api.py
@@ -9,6 +9,7 @@ from api.policies_endpoint import PoliciesEndpoint
 from api.ping_endpoint import PingEndpoint
 from api.cowsay_endpoint import CowsayEndpoint
 from api.openstack_endpoint import OpenstackEndpoint
+from api.dns_endpoint import DNSEndpoint
 from api.satellite_endpoint import SatelliteEndpoint
 from api.scheduler_endpoint import SchedulerEndpoint
 
@@ -33,6 +34,7 @@ class API(object):
         self.ping = PingEndpoint(self.session)
         self.cowsay = CowsayEndpoint(self.session)
         self.me = MeEndpoint(self.session)
+        self.dns = DNSEndpoint(self.session)
         self.satellite = SatelliteEndpoint(self.session)
         self.scheduler = SchedulerEndpoint(self.session)
 
---
+++ tests/features/steps/api/dns_server_endpoint.py
@@ -0,0 +1,82 @@
+import requests
+
+from api.base_endpoint import BaseEndpoint, log_call
+
+
+class DNSServerEndpoint(BaseEndpoint):
+    """
+    Represents the /dns/server API endpoint.
+    """
+
+    UNVERIFIABLE_ITEMS = {
+        'get_list': {},
+        'create': {},
+        'delete': {},
+        'get': {},
+        'update': {}
+    }
+
+    def url(self, suffix: str = '') -> str:
+        return f"{self.base_url}/dns/server{suffix}"
+
+    @log_call(BaseEndpoint.LOGGER, UNVERIFIABLE_ITEMS['get_list'])
+    def get_list(
+        self,
+        filter: dict = None,
+        sort: str = None,
+        page: int = None,
+        limit: int = None
+    ) -> requests.Response:
+        args = self.get_function_arguments(
+            locals(), skip_args=['self', '__class__'])
+        body = self.create_body(args)
+        response = super().get(self.url(), json=body)
+
+        return response
+
+    @log_call(BaseEndpoint.LOGGER, UNVERIFIABLE_ITEMS['create'])
+    def create(
+        self,
+        credentials: str | dict,
+        hostname: str,
+        description: str = None,
+        name: str = None,
+        owner_group_id: str = None,
+        zone: str = None,
+    ) -> requests.Response:
+        args = self.get_function_arguments(locals(), skip_args=['self'])
+        body = self.create_body(args)
+        response = self.post(self.url(), json=body)
+
+        return response
+
+    @log_call(BaseEndpoint.LOGGER, UNVERIFIABLE_ITEMS['delete'])
+    def delete(self, id: int) -> requests.Response:
+        url = self.url(suffix=f"/{id}")
+        response = super().delete(url)
+
+        return response
+
+    @log_call(BaseEndpoint.LOGGER, UNVERIFIABLE_ITEMS['get'])
+    def get(self, id: int) -> requests.Response:
+        url = self.url(suffix=f"/{id}")
+        response = super().get(url)
+
+        return response
+
+    @log_call(BaseEndpoint.LOGGER, UNVERIFIABLE_ITEMS['update'])
+    def update(
+        self,
+        id: int,
+        credentials: str | dict = None,
+        hostname: str = None,
+        description: str = None,
+        name: str = None,
+        owner_group_id: str = None,
+        zone: str = None,
+    ) -> requests.Response:
+        args = self.get_function_arguments(locals(), skip_args=['self', 'id'])
+        body = self.create_body(args)
+        response = self.patch(self.url(suffix=f"/{id}"), json=body)
+
+        return response
