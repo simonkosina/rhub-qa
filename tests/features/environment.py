44957636a971e2613b8407f60430e47c8549c772
skosina <skosina@redhat.com>
2022-10-28 16:09:15 +0200

    API setup and cleanup

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -1,3 +1,5 @@
+# TODO: move 'web' tag to fixture
+
 import time
 import os
 import fixtures
@@ -53,19 +55,35 @@ def before_scenario(context, scenario):
 
 
 def after_scenario(context, scenario):
+    # FIXME: use fixtures for closing the browser
     if 'web' in context.tags:
         context.browser.quit()
+
+    if 'fixture.api' in context.tags:
+        # make sure we have the permissions
+        response = context.api.auth.token.create(context.auth[0], context.auth[1])
+        response.raise_for_status()
+        context.api.update_token(response.json()['access_token'])
+       
+        # restore the environment
+        for cleanup in context.api.logger.cleanups:
+            method = cleanup['method']
+            kwargs = cleanup['kwargs']
+            method(**kwargs)
+
+        context.api.logger.reset_cleanups()
 
 
 def after_all(context):
 
-    time.sleep(5)
-    os.system('./../tools/cm selenoid stop')
-    os.system('./../tools/cm selenoid-ui stop')
-    os.system('cp ~/.aerokube/selenoid/video/*.mp4 ~/Videos/')
+    # TODO: Use only for web tests
+    # time.sleep(5)
+    # os.system('./../tools/cm selenoid stop')
+    # os.system('./../tools/cm selenoid-ui stop')
+    # os.system('cp ~/.aerokube/selenoid/video/*.mp4 ~/Videos/')
 
-    print(" ")
-    print("--------------------------------------------")
-    print(" ")
-    print("Tests results: ")
-    print("    ")
+    print()
+    print("--------------------------------------------------")
+    print()
+    print("Test results:")
+    print()
